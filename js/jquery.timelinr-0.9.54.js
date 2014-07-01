3a4e2dd249cb00b816b777524f2f958514398de5
emdsgn <mansurachowdhury@Mansura-Chowdhurys-iMac.local>
2014-07-01 09:42:36 -0400

    Fixing web

--- js/jquery.timelinr-0.9.54.js
+++ js/jquery.timelinr-0.9.54.js
@@ -71,30 +71,7 @@ jQuery.fn.timelinr = function(options){
 			}
 			$(settings.issuesDiv+' li').animate({'opacity':settings.issuesTransparency},{queue:false, duration:settings.issuesSpeed}).removeClass(settings.issuesSelectedClass).eq(currentIndex).addClass(settings.issuesSelectedClass).fadeTo(settings.issuesTransparencySpeed,1);
 			// prev/next buttons now disappears on first/last issue | bugfix from 0.9.51: lower than 1 issue hide the arrows | bugfixed: arrows not showing when jumping from first to last date
-			if(howManyDates == 1) {
-				$(settings.prevButton+','+settings.nextButton).fadeOut('fast');
-			} else if(howManyDates == 2) {
-				if($(settings.issuesDiv+' li:first-child').hasClass(settings.issuesSelectedClass)) {
-					$(settings.prevButton).fadeOut('fast');
-				 	$(settings.nextButton).fadeIn('fast');
-				} 
-				else if($(settings.issuesDiv+' li:last-child').hasClass(settings.issuesSelectedClass)) {
-					$(settings.nextButton).fadeOut('fast');
-					$(settings.prevButton).fadeIn('fast');
-				}
-			} else {
-				if( $(settings.issuesDiv+' li:first-child').hasClass(settings.issuesSelectedClass) ) {
-					$(settings.nextButton).fadeIn('fast');
-					$(settings.prevButton).fadeOut('fast');
-				} 
-				else if( $(settings.issuesDiv+' li:last-child').hasClass(settings.issuesSelectedClass) ) {
-					$(settings.prevButton).fadeIn('fast');
-					$(settings.nextButton).fadeOut('fast');
-				}
-				else {
-					$(settings.nextButton+','+settings.prevButton).fadeIn('slow');
-				}	
-			}
+			
 			// now moving the dates
 			$(settings.datesDiv+' a').removeClass(settings.datesSelectedClass);
 			$(this).addClass(settings.datesSelectedClass);
@@ -201,7 +178,7 @@ jQuery.fn.timelinr = function(options){
 				$(settings.prevButton+','+settings.nextButton).fadeOut('fast');
 			} else if(howManyDates == 2) {
 				if($(settings.issuesDiv+' li:first-child').hasClass(settings.issuesSelectedClass)) {
-					$(settings.prevButton).fadeOut('fast');
+					;
 				 	$(settings.nextButton).fadeIn('fast');
 				} 
 				else if($(settings.issuesDiv+' li:last-child').hasClass(settings.issuesSelectedClass)) {
@@ -210,7 +187,7 @@ jQuery.fn.timelinr = function(options){
 				}
 			} else {
 				if( $(settings.issuesDiv+' li:first-child').hasClass(settings.issuesSelectedClass) ) {
-					$(settings.prevButton).fadeOut('fast');
+					;
 				} 
 				else if( $(settings.issuesDiv+' li:last-child').hasClass(settings.issuesSelectedClass) ) {
 					$(settings.nextButton).fadeOut('fast');
